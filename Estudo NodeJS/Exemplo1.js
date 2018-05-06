14b35630c959640d9fe0900a1334aa9d0013aae8
HenriqueBraz <38267870+HenriqueBraz@users.noreply.github.com>
2018-05-06 20:02:36 -0300

    Update Exemplo1.js

--- Estudo NodeJS/Exemplo1.js
+++ Estudo NodeJS/Exemplo1.js
@@ -1,3 +1,11 @@
+Este método cria uma página básica no servidor local. A palavra reservada "require" é o equivalente em Java ao "import", servindo
+para chamar as bibliotecas (neste caso a http). Temos declarados 4 variáveis; a  http, que recebe a biblioteca,  a 
+server, que recebe a função da http create server, cujo os parâmetros são recebidos pelas variaveis req (requisição) e
+res (resposta).
+
+```
+// inicio do método
+
 var http = require('http');
 //req = request
 //res = response
@@ -9,12 +17,20 @@ var server = http.createServer(function(req,res){
 		res.write('<br>');
 		res.write('Aulas de Nodejs');
 		res.write('<br>');
-		res.write ('Em 05/05/2018');
+		res.write ('Em 06/05/2018');
 		
 		res.end();
 });
-	
+
+// fim do método
+
+//chamada do método
+
 server.listen (3000, function(){
 	
 	console.log('Servidor rodando na porta 3000');
 });
+```
+Quando chamamos a função .listen dentro de server, setamos os parâmetros para a porta 3000 do servidor, sendo que se está ok ela retorna,
+ainda dentro do console, a frase "Servidor rodando na porta 3000". Para verificar a página criada, basta ir no navegador e 
+digitar: localhost:3000
